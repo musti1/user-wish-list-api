709a59905f419c718f51b9fe3f36faaba454d4c6
Mustafa Haider <mustafi51992@gmail.com>
2019-09-07 05:18:41 +0500

    fix wishlist route

--- app/Http/Routes/wishList.js
+++ app/Http/Routes/wishList.js
@@ -3,7 +3,7 @@ const router = express.Router({});
 
 const WishListController = require('../Controllers/WishListController');
 
-router.get('/create', WishListController.create);
+router.post('/create', WishListController.create);
 router.get('/list', WishListController.allWishLists);
 router.get('/delete/:wishListId', WishListController.delete);
 router.get('/wish-list/:wishListId', WishListController.singleWishList);
